9234e528390d8259f9c04504017fef2d79f143df
Chris Monogios <chr_mon_@hotmail.com>
2017-01-10 21:41:08 +0100

    initial commit, based on the guideline of https://www.typescriptlang.org/docs/handbook/react-&-webpack.html, do not include the .js and .js.map files

---
+++ Boilerplate/javascript/components/FirstComponent.tsx
@@ -0,0 +1,11 @@
+﻿import * as React from "react";
+
+export interface FirstComponentProps
+{
+}
+
+export class FirstComponent extends React.Component<FirstComponentProps, undefined> {
+    render() {
+        return <h1>First component</h1>;
+    }
+}
---
+++ Boilerplate/javascript/index.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { FirstComponent } from "./components/FirstComponent";
+
+ReactDOM.render(
+    <FirstComponent />,
+    document.getElementById("example")
+);

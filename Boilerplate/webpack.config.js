1bda397d1f24b202f3a612645e2a3bdf70210358
Chris Monogios <chr_mon_@hotmail.com>
2017-01-11 16:00:21 +0100

    watch and build when changes, dev-server for live development

---
+++ Boilerplate/webpack.config.js
@@ -0,0 +1,44 @@
+﻿var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+    entry: [
+        path.resolve(__dirname, "javascript/index.tsx")
+    ],
+
+    output: {
+        path: path.join(__dirname, "distribution"),
+        filename: "bundle.js",
+    },
+
+    devtool: "source-map", // Enable sourcemaps for debugging webpack's output.
+
+    devServer: {
+        contentBase: "distribution"
+    },
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        loaders: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+        ],
+        preLoaders: [
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+}
